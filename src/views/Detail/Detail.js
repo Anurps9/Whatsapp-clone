1ed3abb0a05165ffc6e933af6ea98ddacc3aea34
singhsachin17 <singhsachin171999@gmail.com>
2021-07-15 18:54:45 +0530

    Styled details page

--- src/views/Detail/Detail.js
+++ src/views/Detail/Detail.js
@@ -1,12 +1,17 @@
+import "./Details.css";
 function Detail(props) {
     return (
-      <div
-        className="align"
-        style={{ width: "90%", zIndex: 5, padding: "5%", height: "30px" }}
-      >
-        <img className="align"src={props.contactImg} align="left" alt="Contact-Pic" />
-        <div className="OtherUserNumber">{props.contactNumber}</div>
-        <div className="OtherUserLast">{props.lastMessage}</div>
+      <div style={{ width: "90%", zIndex: 5, padding: "5%", height: "30px" }}>
+        <img
+          className="align"
+          src={props.contactImg}
+          align="left"
+          alt="Contact-Pic"
+        />
+        <div>
+          <p className="OtherUserNumber">{props.contactNumber}</p>
+          <p className="OtherUserLast">{props.lastMessage}</p>
+        </div>
       </div>
     );
   
