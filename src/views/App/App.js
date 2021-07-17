7cfe501e13c107d049337f9a2df66b00f47e366d
Anurag Sisodiya <anurps9@gmail.com>
2021-07-17 12:40:38 +0530

    Add UserDescriptionScreen

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import DirectChatScreen from '../DirectChatScreen/DirectChatScreen.js';
-import Header from '../Header/Header.js';
-import Footer from '../Footer/Footer.js';
-import Detail from '../Detail/Detail.js';
+import UserDescriptionScreen from '../UserDescriptionScreen/UserDescriptionScreen.js';
 
 function App() {    
    return (
-     <div className="App" s>
-       <DirectChatScreen /> 
+     <div className="App">
+       <UserDescriptionScreen /> 
      </div>
    );
 }
